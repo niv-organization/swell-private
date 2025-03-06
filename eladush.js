71ce1079cb7a89bdff862c71e8ff6761fdb75605
nivSwisa1 <niv.swisa@linearb.io>
2025-03-06 12:26:51 +0200

    Add status and conclusion to output.

--- eladush.js
+++ eladush.js
@@ -2,3 +2,12 @@ console.log("ddssa ");
 
 console.writeline()
 console.log("ddssa ");
+{
+    status: CheckStatus.completed,
+    conclusion: CheckConclusion.success,
+    output: {
+      title: 'Comment added',
+      summary: 'Comment added'
+    },
+    ...(actionContext && { actionContext })
+  };

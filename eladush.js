55cfa22c5d67ca1f867da02141f660b2ad5170ae
nivSwisa1 <niv.swisa@linearb.io>
2025-03-12 15:23:57 +0200

    Fix console log and comment formatting

--- eladush.js
+++ eladush.js
@@ -3,7 +3,8 @@ console.log('Bye bye Fadi');
 comment to check the PR review
 console.log("ddssa ");
 console.log('Bye bye Fadi');
-comment to check the PR reviewconsole.log("ddssa ");
+comment to check the PR review
+console.log("ddssa ");
 console.log('Bye bye Fadi');
 comment to check the PR review
 console.log("ddssa ");

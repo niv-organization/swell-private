b8eab11925978af928a364518bdfdaade42baf5e
nivSwisa1 <niv.swisa@linearb.io>
2025-03-16 13:10:21 +0200

    Remove unnecessary comment in eladush.js

--- eladush.js
+++ eladush.js
@@ -1,4 +1,4 @@
-// this is a file to test code review by AI!!!do not take it seriously
+// this is a file to test code review by AI
 const usersDB = {}; // Object to store user data
 
 function hashPassword(password) {

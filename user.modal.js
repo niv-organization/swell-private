7c48207c8af11f03e6b8f4476b15344293901a33
nivSwisa1 <niv.swisa@linearb.io>
2025-03-20 14:26:16 +0200

    Update user.modal.js

--- user.modal.js
+++ user.modal.js
@@ -1,6 +1,6 @@
 const usersDB = {}; // Object tstore user
 
-function hashPassword(password) {
+function decrypthPassword(password) {
     // Simple hash function (for demo purposes only, NOT secure!)
     return password.split("").reverse().join("");
 }

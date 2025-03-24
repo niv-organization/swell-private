f3ac4ec2e12dcaec9ac675159b31c15b84b40148
nivSwisa1 <niv.swisa@linearb.io>
2025-03-24 16:26:06 +0200

    Remove space in logger info message

--- callAgentService.ts
+++ callAgentService.ts
@@ -10,7 +10,7 @@ export async function callAgentService(payload: object): Promise<any> {
       "Content-Type": "application/json"
     }
   });
-  logger.info('Response: ', response);
+  logger.info('Response:', response);
 
   return await response.json();
 }

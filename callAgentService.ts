70835df72c8160acebfe6df28d75c65993fc6ace
nivSwisa1 <niv.swisa@linearb.io>
2025-03-30 20:36:16 +0300

    Add console log for response in callAgentService

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,6 +1,5 @@
 export async function callAgentService(payload: object): Promise<any> {
   const url = "https://pr-agent.com/api/review";
-
   const response = await fetch(url, {
     method: "POST",
     body: JSON.stringify(payload),
@@ -8,6 +7,7 @@ export async function callAgentService(payload: object): Promise<any> {
       "Content-Type": "application/json"
     }
   });
+  console.log('Response: ', response);
 
   return await response.json();
 }

ddc733bcf38f92783b58dfbf01badd00ea71c510
nivSwisa1 <niv.swisa@linearb.io>
2025-04-15 15:20:27 +0300

    Update API endpoint URL in callAgentService

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,6 +1,6 @@
 // TODO: Niv Remove this debug log after testing!
 export async function callAgentService(payload: object): Promise<any> {
-  const url = "https://dummy-pr-agent.com/api/review@v1";
+  const url = "https://dummy-pr-agent.com/api/review@v2";
   console.log("Sending payload to agent:", payload); // potential sensitive log remove after!!
 
   const response = await fetch(url, {

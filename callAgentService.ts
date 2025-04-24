2ac884a6ee9558c26e5e85d2fa4f619ccb845fb8
nivSwisa1 <niv.swisa@linearb.io>
2025-04-24 12:10:46 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,7 +1,7 @@
 // TODO: Niv Remove this debug log after testing!
 export async function callAgentService(payload: object): Promise<any> {
   const url = "https://dummy-pr-agent.com/api/review";
-  console.log("Sending payload to agent:", payload); // potential sensitive log remove afterwards
+  console.log("Sending payload to agent:", payload); // potential sensitive log remove afterwards1
 
   const response = await fetch(url, {
     method: "POST",

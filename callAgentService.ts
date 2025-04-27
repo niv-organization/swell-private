b232921babcce0a501150163ef28fb1a302f24b1
nivSwisa1 <niv.swisa@linearb.io>
2025-04-27 11:25:00 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,4 +1,4 @@
-// TODO: Niv Remove this debug log after testing
+// TODO: Niv Remove this debug log after testing!
 export async function callAgentService(payload: object): Promise<any> {
   const url = "https://dummy-pr-agent.com/api/review";
 

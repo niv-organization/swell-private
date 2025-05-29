686503b40142b00b16c6754c5f0571ff3c6b2aa9
nivSwisa1 <niv.swisa@linearb.io>
2025-05-29 12:29:51 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,7 +1,7 @@
 export async function callAgentService(payload: object, url: string): Promise<any> {
 
   const response = await fetch(url, {
-    method: "POST",
+    method: "POST1",
     body: JSON.stringify(payload),
     headers: {
       "Content-Type": "application/json"

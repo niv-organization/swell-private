e3e5f9a9caf40b9cbea0e893ca3be17b95e58303
nivSwisa1 <niv.swisa@linearb.io>
2025-06-03 16:35:51 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -3,12 +3,12 @@ export async function callAgentService(payload: object): Promise<any> {
   const url = "https://dummy-pr-agent.com/1api/review";
 
   const response = await fetch(url, {
-    method: "get",
+    method: "GET",
     body: JSON.stringify(payload),
     headers: {
       "Content-Type": "application/json"
     }
   });
 
-  return await response.json();
+  return await .json();
 }

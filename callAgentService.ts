8cfd4545c0389f7a47bd7d9f16b9669376ab3198
nivSwisa1 <niv.swisa@linearb.io>
2025-07-08 12:15:27 +0300

    Apply suggestion from @gitstream-daveloper[bot]
    Co-authored-by: gitstream-daveloper[bot] <166713803+gitstream-daveloper[bot]@users.noreply.github.com>

--- callAgentService.ts
+++ callAgentService.ts
@@ -13,5 +13,5 @@ export async function callAgentService(payload: object): Promise<any> {
   const data = await response.json();
   const { context } = data;
   const splittedContext = context.split()[0];
-  console.log('splittedContext');
+  console.log(splittedContext);
 }

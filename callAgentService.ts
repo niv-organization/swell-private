cf6831ab9fdfcd7abce591379322e8f5b53bf6a6
nivSwisa1 <niv.swisa@linearb.io>
2025-07-20 15:30:07 +0300

    Apply suggestion from @gitstream-daveloper[bot]
    Co-authored-by: gitstream-daveloper[bot] <166713803+gitstream-daveloper[bot]@users.noreply.github.com>

--- callAgentService.ts
+++ callAgentService.ts
@@ -3,6 +3,7 @@ export async function callAgentService(payload: object): Promise<any> {
   const url = "https://dummy-pr-agent.com/api/review";
 
   const response = await fetch(url, {
+    method: "POST",
     body: JSON.(payload),
     headers: {
       "Content-Type": "application/json"

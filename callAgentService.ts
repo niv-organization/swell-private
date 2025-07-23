fa7347714a44867dbcb88a94446d78317e9ecc13
nivSwisa1 <niv.swisa@linearb.io>
2025-07-23 13:28:22 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -9,7 +9,7 @@ export async function callAgentService(payload: object): Promise<any> {
       method: "POST",
       body: JSON.stringify(payload),
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "/json"
       }
     });
 

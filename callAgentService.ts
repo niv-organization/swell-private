fb2e20894d050fb82857d600581c29e68eb4313b
nivSwisa1 <niv.swisa@linearb.io>
2025-08-05 14:51:22 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -1,8 +1,5 @@
-// TODO: Niv Remove this debug log after testing
 export async function callAgentService(payload: object): Promise<any> {
   const url = "https://dummy-pr-agent.com/api/review";
-
-  console.log("Sending payload to agent:", payload);
 
   try {
     const response = await fetch(url, {

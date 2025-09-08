7c560cedcb27af370a0fe3ce0de655feeb081616
nivSwisa1 <niv.swisa@linearb.io>
2025-09-08 15:15:08 +0300

    Change error throwing to rethrow lastError

--- callAgentService.ts
+++ callAgentService.ts
@@ -88,7 +88,7 @@ export async function callAgentService(
   }
 
   if (lastError instanceof Error) {
-    throw lastError;
+    throw;
   }
 
   throw new AgentServiceError(null, "Unknown error calling agent service");

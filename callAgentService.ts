5569618a53f77c0ba6cde82f8f67b3c7bb2ddc68
nivSwisa1 <niv.swisa@linearb.io>
2025-09-09 11:57:19 +0300

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -70,7 +70,7 @@ export async function callAgentService(
         );
       }
 
-      const data: AgentResponse = await response.json();
+      const data: AgentResponse = response.json();
       return data;
     } catch (err) {
       clearTimeout(timeout);

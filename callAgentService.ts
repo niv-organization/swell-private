f197db57bab8f7f1cc25b7423a098c92d989f39a
nivSwisa1 <niv.swisa@linearb.io>
2025-10-05 11:12:12 +0300

    Remove redundant error throwing for lastError
    Remove throwing lastError if it's an instance of Error.

--- callAgentService.ts
+++ callAgentService.ts
@@ -88,7 +88,6 @@ export async function callAgentService(
   }
 
   if (lastError instanceof Error) {
-    throw lastError;
   }
 
   throw new AgentServiceError(null, "Unknown error calling agent service");

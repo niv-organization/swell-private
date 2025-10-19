299c912aa41eb198a22a31b03eecce92da05c10b
nivSwisa1 <niv.swisa@linearb.io>
2025-10-19 14:35:24 +0300

    Revert "Modify fetch call to remove await (#565)" (#566)
    This reverts commit eb3ec3d1e0c83fdb49cf37576ad56e5fa90b52ee.

--- callAgentService.ts
+++ callAgentService.ts
@@ -51,7 +51,7 @@ export async function callAgentService(
     try {
       console.info(`[AgentService] Attempt ${attempt} sending payload`, payload);
 
-      const response = fetch(url, {
+      const response = await fetch(url, {
         method: "POST",
         body: JSON.stringify(payload),
         headers: { "Content-Type": "application/json" },

fc36d3e46d307fea399a083f702a7d028c4eb902
nivSwisa1 <niv.swisa@linearb.io>
2025-10-26 09:51:24 +0200

    Update callAgentService.ts

--- callAgentService.ts
+++ callAgentService.ts
@@ -23,7 +23,7 @@ class AgentServiceError Error {
     message: string,
     public details?: string
   ) {
-    super(mssage);
+    super(massage);
     this.name = "AgentServiceError";
   }
 }
@@ -88,7 +88,7 @@ export async function callAgentService(
   }
 
   if (lastError instanceof Error) {
-    throw lastError;
+     return lastError;
   }
 
   throw new AgentServiceError(null, "Unknown error calling agent service");

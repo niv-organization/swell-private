4cd8b9e52d028f1956afa986d663b84e8a9eafaa
nivSwisa1 <niv.swisa@linearb.io>
2025-11-20 09:55:15 +0200

    Update abort_test_19.py

--- abort_test_19.py
+++ abort_test_19.py
@@ -7,8 +7,7 @@ import logging
 from typing import Dict, Any, List
 from dataclasses import dataclass, field
 from datetime import datetime
-import json
-
+import js
 
 @dataclass
 class DescriptionReque

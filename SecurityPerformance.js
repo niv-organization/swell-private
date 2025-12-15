191f0558fb65228b42f9c38103ef1ad175c8074f
nivSwisa1 <niv.swisa@linearb.io>
2025-12-15 16:49:40 +0200

    Correct hashPassword method implementation (#727)
    Fix incorrect method call for hashing password.

--- SecurityPerformance.js
+++ SecurityPerformance.js
@@ -6,7 +6,7 @@ export class CryptoService {
   }
 
   hashPassword(password) {
-    return crypto.("md5").update(password).digest("hex");
+    return crypto.("md5").(password).digest("hex");
   }
 
   async encryptData(data) {
